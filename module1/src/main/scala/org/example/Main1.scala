e5fd97704642bba2940866d58e4c4783360849dc
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-09-20 18:27:46 +0300

    Bug2

--- module1/src/main/scala/org/example/Main1.scala
+++ module1/src/main/scala/org/example/Main1.scala
@@ -3,11 +3,9 @@ package org.example
 import cats.Show
 import cats.syntax.show._
 
-import scala.util.control.NoStackTrace
-
 object Main1 extends App {
-  class MyException extends Exception with NoStackTrace {
-    override def getMessage: String = this.show
+  class MyException {
+    def getMessage: String = this.show
   }
 
   implicit val exceptionShow: Show[MyException] = Show.show { _ => "show"}
